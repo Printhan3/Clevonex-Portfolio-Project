9c630b1809af128138711c8fe9012bd480077493
Vigneswaran Printhan <94120170+Printhan3@users.noreply.github.com>
2024-05-06 11:58:26 +0300

    Update Pricing.jsx

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -11,31 +11,37 @@ const Pricing = () => {
 
 
      
-    <div class="bg-[url('../src/assets/Por6.jpg')] bg-cover rounded-lg shadow-md p-6" >
-      <h2 class="text-xl font-bold text-white mb-4">General Prices:</h2>
-      <ul class="text-white">
-        <li  class="flex justify-between py-2 border-b border-gray-700">
+    <div className="bg-[url('../src/assets/Por6.jpg')] bg-cover rounded-lg shadow-md p-6" >
+      <h2 className="text-xl font-bold text-white mb-4">General Prices:</h2>
+      <ul className="text-white">
+        <li  className="flex justify-between py-2 ">
           <span >Eye Makeup</span>
+          <span className='font-bold'>---------------------------------------</span>
           <span>$20</span>
         </li>   
-        <li class="flex justify-between py-2 border-b border-gray-700">
+        <li className="flex justify-between py-2 ">
           <span>Facial Makeup</span>
+          <span className='font-bold'>---------------------------------------</span>
           <span>$60</span>
         </li>
-        <li class="flex justify-between py-2 border-b border-gray-700">
+        <li className="flex justify-between py-2">
           <span>Bridal Makeup</span>
+          <span className='font-bold'>---------------------------------------</span>
           <span>$200</span>
         </li>
-        <li class="flex justify-between py-2 border-b border-gray-700">
+        <li className="flex justify-between py-2 ">
           <span>Child Face Painting Makeup</span>
+          <span className='font-bold'>----------------------</span>
           <span>$25</span>
         </li>
-        <li class="flex justify-between py-2 border-b border-gray-700">
+        <li className="flex justify-between py-2">
           <span>Effect Makeup</span>
+          <span className='font-bold'>---------------------------------------</span>
           <span>$100</span>
         </li>
-        <li class="flex justify-between py-2">
+        <li className="flex justify-between py-2">
           <span>Hair Makeup</span>
+          <span className='font-bold'>---------------------------------------</span>
           <span>$40</span>
         </li>
       </ul>
@@ -43,31 +49,37 @@ const Pricing = () => {
 
      
      
-    <div class="bg-[url('../src/assets/Por5.jpg')] bg-cover rounded-lg shadow-md p-6">
-      <h2 class="text-xl font-bold text-white mb-4">Bridal Hairdo:</h2>
-      <ul class="text-white">
-        <li class="flex justify-between py-2 border-b border-gray-700">
+    <div className="bg-[url('../src/assets/Por5.jpg')] bg-cover rounded-lg shadow-md p-6">
+      <h2 className="text-xl font-bold text-white mb-4">Bridal Hairdo:</h2>
+      <ul className="text-white">
+        <li className="flex justify-between py-2">
           <span>Eye Makeup</span>
+          <span className='font-bold'>---------------------------------------</span>
           <span>$20</span>
         </li>
-        <li class="flex justify-between py-2 border-b border-gray-700">
+        <li className="flex justify-between py-2">
           <span>Facial Makeup</span>
+          <span className='font-bold'>---------------------------------------</span>
           <span>$60</span>
         </li>
-        <li class="flex justify-between py-2 border-b border-gray-700">
+        <li className="flex justify-between py-2">
           <span>Bridal Makeup</span>
+          <span className='font-bold'>---------------------------------------</span>
           <span>$200</span>
         </li>
-        <li class="flex justify-between py-2 border-b border-gray-700">
+        <li className="flex justify-between py-2">
           <span>Child Face Painting Makeup</span>
+          <span className='font-bold'>----------------------</span>
           <span>$25</span>
         </li>
-        <li class="flex justify-between py-2 border-b border-gray-700">
+        <li className="flex justify-between py-2">
           <span>Effect Makeup</span>
+          <span className='font-bold'>---------------------------------------</span>
           <span>$100</span>
         </li>
-        <li class="flex justify-between py-2">
+        <li className="flex justify-between py-2">
           <span>Hair Makeup</span>
+          <span className='font-bold'>---------------------------------------</span>
           <span>$40</span>
         </li>
       </ul>
@@ -79,3 +91,6 @@ const Pricing = () => {
 }
 
 export default Pricing;
+
+
+
