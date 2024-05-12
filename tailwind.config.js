cee86515e8c0a20e2d94b6eea0b4674f9bcddb57
Vigneswaran Printhan <94120170+Printhan3@users.noreply.github.com>
2024-05-12 19:03:39 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'didact': ['didact ', 'sans-serif'], 
+        'Oswald' : ['oswald', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
